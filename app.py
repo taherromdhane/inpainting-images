a4009494c4eb0e39c9f17670269478a41f9ba9de
taherromdhane <romdhane.attaher@gmail.com>
2020-10-11 20:11:49 +0200

    finished notebook, script and interface

---
+++ app.py
@@ -0,0 +1,69 @@
+import dash
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+from dash_canvas import DashCanvas
+import json
+from dash_table import DataTable
+from PIL import Image
+import base64
+import os
+import sys
+import matplotlib.pyplot as plt
+
+from utils import getMask
+from inpaint import inpaint
+
+app = dash.Dash(__name__)
+
+filename = 'Capture.PNG'
+canvas_width = 600
+width = canvas_width
+
+app.layout = html.Div([
+    html.H6('Draw on image and press Save to show annotations geometry'),
+    DashCanvas(id='annot-canvas',
+               lineWidth=5,
+               filename=app.get_asset_url(filename),
+               width=canvas_width,
+               ),
+    html.Img(id='image', width=canvas_width)])
+
+@app.callback(Output('image', 'src'), [Input('annot-canvas', 'json_data')])
+def update_image(string):
+    if string:
+        data = json.loads(string)
+    else:
+        raise PreventUpdate
+
+    print(data['objects'], file = sys.stderr)
+
+    image_width = int(data['objects'][0]["width"])
+    image_height = int(data['objects'][0]["height"])
+
+    left = int(data['objects'][1]["left"] * image_width / canvas_width)
+    top = int(data['objects'][1]["top"] * image_width / canvas_width)
+    width = int(data['objects'][1]["width"] * image_width / canvas_width)
+    height = int(data['objects'][1]["height"] * image_width / canvas_width)
+
+    mask = getMask(image_width, image_height, left, top, width, height)
+
+    plt.imsave(os.getcwd() + app.get_asset_url("mask.jpg"), mask)
+
+    image_filename = 'Capture.PNG'
+    image = Image.open(os.getcwd() + app.get_asset_url(image_filename))
+
+    inpainted = inpaint(image, mask, local_radius=50)
+
+    inpainted_filename = "inpaintedf.PNG"
+
+    # inpainted_array = Image.fromarray(inpainted)
+    # Image.save(inpainted, "/assets/inpainted.jpeg"
+
+    plt.imsave(os.getcwd() + app.get_asset_url(inpainted_filename), inpainted)
+
+    return app.get_asset_url(inpainted_filename)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, dev_tools_hot_reload = False)
