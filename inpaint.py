70aaad3369fbe355ea3663f025c7bf72d5b929d2
Taher Romdhane <taher@veamly.com>
2020-10-27 21:37:48 +0100

    added data term

--- inpaint.py
+++ inpaint.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.ndimage.filters import convolve
+from skimage.color import rgb2gray
 
 def normalize(image) :
     return image/np.max(image)
@@ -38,9 +40,67 @@ def patchConfidence(center, confidence, mask, patch_size) :
     offset = patch_size//2
     
     return np.sum(confidence[i - offset : i + offset + 1, j - offset : j + offset + 1]) / patch_size**2
+    
+def calcNormalMatrix(center, mask):
+    
+    x_kernel = np.array([[.25, 0, -.25], [.5, 0, -.5], [.25, 0, -.25]])
+    y_kernel = np.array([[.25, .5, .25], [0, 0, 0], [-.25, -.5, -.25]])
+    
+    x_normal = convolve(mask.astype(float), x_kernel)
+    y_normal = convolve(mask.astype(float), y_kernel)
+    
+    normal = np.dstack((x_normal, y_normal))[1, 1, :]
+    
+    norm = np.sqrt(normal[0]**2 + normal[1]**2)
+    if norm == 0 :
+        norm = 1
 
-def patchData(center, image, mask, alpha) :
-    return 1
+    unit_normal = -normal/norm
+    return unit_normal
+    
+def calcGradientMatrix(center, image, mask, patch_size):
+    
+    # TODO: find a better method to calc the gradient
+    height, width = image.shape[:2]
+    
+    
+    grey_image = rgb2gray(image)
+    grey_image[mask == 1] = None
+
+    gradient = np.nan_to_num(np.array(np.gradient(grey_image)))
+    gradient_val = np.sqrt(gradient[0]**2 + gradient[1]**2)
+    max_gradient = np.zeros([2])
+    
+    offset = patch_size//2
+    i, j = center
+    
+    patch_y_gradient = gradient[0][i - offset : i + offset + 1, j - offset : j + offset + 1]
+    patch_x_gradient = gradient[1][i - offset : i + offset + 1, j - offset : j + offset + 1]
+    patch_gradient_val = gradient_val[i - offset : i + offset + 1, j - offset : j + offset + 1]
+
+    patch_max_pos = np.unravel_index(
+        patch_gradient_val.argmax(),
+        patch_gradient_val.shape
+    )
+
+    max_gradient[0] = patch_y_gradient[patch_max_pos]
+    max_gradient[1] = patch_x_gradient[patch_max_pos]
+
+    return max_gradient
+    
+    
+def patchData(center, image, mask, alpha, patch_size) :
+    
+    offset = 1
+    i, j = center
+    patch_mask = mask[i - offset : i + offset + 1, j - offset : j + offset + 1]
+    normal = calcNormalMatrix(center, patch_mask)
+    max_gradient = calcGradientMatrix(center, image, mask, patch_size)
+    
+    data = np.abs(np.dot(normal, max_gradient.T)) / alpha
+    
+    return data
+   
 
 def getMaxPriority(border_pxls, confidence, image, mask, alpha, patch_size) :
     
@@ -56,8 +116,9 @@ def getMaxPriority(border_pxls, confidence, image, mask, alpha, patch_size) :
             continue
         
         current_Cp = patchConfidence(pixel, confidence, mask, patch_size)
-        current_Dp = patchData(pixel, image, mask, alpha)
-
+        # current_Dp = patchData(pixel, image, mask, alpha, patch_size)
+        current_Dp = 1
+        
         current_Pp = current_Cp * current_Dp # Pp to change into matrix
         
         if current_Pp > Pp :
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def getMask(image_width, image_height, left, top, width, height) :
 
-    mask = np.ones((image_width, image_height))
+    mask = np.ones((image_height, image_width))
 
     mask[top : top + height + 1, left : left + width + 1] = 0
 
