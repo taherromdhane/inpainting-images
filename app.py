e618b78287b8fc023baf9a4de742efcb10e0744f
taherromdhane <romdhane.attaher@gmail.com>
2020-11-14 20:39:07 +0100

    added selectors and upload feature

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ import json
 from dash_table import DataTable
 from dash_canvas.utils import parse_jsonstring
 import dash_core_components as dcc
-from PIL import Image
+from PIL import Image, ImageOps
 import base64
 import os
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 import cv2
 import uuid 
@@ -19,14 +20,40 @@ from utils import getMask
 from inpaint import inpaint
 
 app = dash.Dash(__name__, serve_locally=False)
-
-filename = 'test_images/pavage.png'
-canvas_width = 400
+canvas_width = 500
 width = canvas_width
 
 app.layout = html.Div([
+    html.H2(
+        'Draw on image and press Save to show annotations geometry',
+        style = {
+            'text-align':'center',
+            'margin' : '40px'
+        }),
     html.Div([
-        html.H2('Draw on image and press Save to show annotations geometry'),
+        dcc.Upload(
+            id = 'upload-image',
+            children = html.Div([
+                'Drag and Drop or ',
+                html.A('Select Images')
+            ]),
+            style = {
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+                'position': 'center'
+            },
+            accept = '.png, .jpg, .jpeg',
+            # don't allow multiple files to be uploaded
+            multiple = False
+        )
+    ]),
+    html.Div([
         html.Div([
             html.H6(children=['Brush width']),
             dcc.Slider(
@@ -36,7 +63,9 @@ app.layout = html.Div([
                 step=1,
                 value=10
             ),
-        ], className="three columns"),
+        ], 
+        className = "three columns",
+        style = {'text-align':'center'}),
         html.Div([
             html.H6(children=['Patch Size']),
             dcc.Slider(
@@ -48,7 +77,9 @@ app.layout = html.Div([
                 marks={3:'3', 5:'5', 7:'7', 9:'9', 11:'11'},
                 included=False
             ),
-        ], className="three columns"),
+        ], 
+        className = "three columns",
+        style = {'text-align':'center'}),
         html.Div([
             html.H6(children=['Local Radius']),
             dcc.Slider(
@@ -56,95 +87,227 @@ app.layout = html.Div([
                 min=50,
                 max=500,
                 step=5,
-                value=50,
+                value=100,
                 marks={3:'3', 5:'5', 7:'7', 9:'9', 11:'11'},
                 included=False
             ),
             html.Div(id='local-radius-output')
-        ], className="two columns")
-    ], className="row"),
+        ], 
+        className = "two columns"),
+        html.Div([
+            html.H6(children=['Data Term significance']),
+            dcc.Slider(
+                id='data-term-slider',
+                min=0,
+                max=1,
+                step=0.01,
+                value=1,
+                marks={0:'0', 1:'1'}
+            ),
+        ],
+        className = "two columns",
+        style = {'text-align' : 'center', 'display' : 'none'})
+    ], 
+    className = "row",
+    style = {'text-align':'center'}),
     html.Div([
         html.Div([
             html.H3('Image to be filled'),
             DashCanvas(
                 id='annot-canvas',
                 lineWidth=10,
-                filename=app.get_asset_url(filename),
                 width=canvas_width,
                 lineColor='rgba(255, 0, 0, 0.6)',
                 tool="rectangle",
                 hide_buttons=['line', 'zoom', 'pan', 'select'],
                 goButtonTitle="Fill Mask"
-                )], 
-            className="six columns"),
+                ),
+            dcc.Checklist(
+                id='fill-rect',
+                options=[
+                    {'label': 'Fill Rectangles', 'value': 'fill'},
+                ],
+                value=['fill']
+                ),
+            html.Div([
+                dcc.Upload(
+                    id = 'upload-mask',
+                    children = html.Div([
+                        'Or ',
+                        html.A('Upload a Mask')
+                    ]),
+                    style = {
+                        'width': '80%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px',
+                        'position': 'center'
+                    },
+                    accept = '.png, .jpg, .jpeg',
+                    # don't allow multiple files to be uploaded
+                    multiple = False
+                )
+            ])
+            ], 
+            className = "six columns",
+            id = "canvas_div"),
         html.Div([
             html.H3('Result'),
-            html.Img(id='image', width=canvas_width)
-        ], className="six columns")
-    ], className="row")
-    ])
+            html.Img(
+                id='image', 
+                width=canvas_width
+            )
+        ], 
+        className = "six columns")
+    ], 
+    className = "row")
+    ],
+    style = {'margin' : '20px'})
 
+# Utility function to display contents with uploaded image
+def save_image(contents, filename):
+    
+    data = contents.encode("utf8").split(b";base64,")[1]
+
+    with open(os.getcwd() + app.get_asset_url(filename), "wb") as fp:
+        fp.write(base64.decodebytes(data))  
+
+# Upload callback
+@app.callback(Output('canvas_div', 'children'),
+              [Input('upload-image', 'contents')],
+              [State('upload-image', 'filename'),
+               State('upload-image', 'last_modified')])
+def update_output(image_content, name, date):
+    
+    if image_content is not None:
+        
+        print(name)
+
+        image_ID = str(uuid.uuid1())
+        image_filename = 'source/' + image_ID + '.png'
+        save_image(image_content, image_filename)
+
+        return  html.Div([
+                    html.H3('Image to be filled'),
+                    DashCanvas(
+                        id='annot-canvas',
+                        lineWidth=10,
+                        width=canvas_width,
+                        lineColor='rgba(255, 0, 0, 0.6)',
+                        filename=app.get_asset_url(image_filename),
+                        tool="rectangle",
+                        hide_buttons=['line', 'zoom', 'pan', 'select'],
+                        goButtonTitle="Fill Mask"
+                        ),
+                    dcc.Checklist(  
+                        id='fill-rect',
+                        options=[
+                            {'label': 'Fill Rectangles', 'value': 'fill'},
+                        ],
+                        value=['fill']),
+                    html.Div([
+                        dcc.Upload(
+                            id = 'upload-mask',
+                            children = html.Div([
+                                'Or ',
+                                html.A('Upload a Mask')
+                            ]),
+                            style = {
+                                'width': '80%',
+                                'height': '60px',
+                                'lineHeight': '60px',
+                                'borderWidth': '1px',
+                                'borderStyle': 'dashed',
+                                'borderRadius': '5px',
+                                'textAlign': 'center',
+                                'margin': '10px',
+                                'position': 'center'
+                            },
+                            accept = '.png, .jpg, .jpeg',
+                            # don't allow multiple files to be uploaded
+                            multiple = False
+                        )
+                    ])
+                    ], 
+                    className = "six columns")
+    else:
+        raise PreventUpdate
+    
+# Callbacks for inpainting parameters 
 @app.callback(Output('annot-canvas', 'lineWidth'),
             [Input('brush-width-slider', 'value')])
 def update_canvas_linewidth(value):
     return value
 
 @app.callback(
-    dash.dependencies.Output('local-radius-output', 'children'),
-    [dash.dependencies.Input('local-radius-slider', 'value')])
+    Output('local-radius-output', 'children'),
+    [Input('local-radius-slider', 'value')])
 def update_output(value):
     return '{}px'.format(value)
 
-@app.callback(Output('image', 'src'), [Input('annot-canvas', 'json_data'), Input('patch-size-slider', 'value')])
-def update_image(string, patch_size):
+# Main callback for the inpainting
+def mask_from_data(string, data, mask_filename, image_width, image_height, canvas_width, rect_fill) :
+    mask = np.ones((image_height, image_width))
+
+    if 'fill' in rect_fill : 
+        for object in data['objects'] :
+            if object["type"] == "rect" :
+                    
+                left = int(object["left"] * image_width / canvas_width)
+                top = int(object["top"] * image_width / canvas_width)
+                width = int(object["width"] * image_width / canvas_width)
+                height = int(object["height"] * image_width / canvas_width)
+
+                rect_mask = getMask(image_width, image_height, left, top, width, height)
+                mask[rect_mask == 0] = 0
+    
+    mask[parse_jsonstring(string, (image_height, image_width)) == 1] = 0 
+
+    plt.imsave(os.getcwd() + app.get_asset_url(mask_filename), mask, cmap=cm.gray)
+
+@app.callback(
+    Output('image', 'src'), 
+    [Input('annot-canvas', 'json_data'),
+    Input('annot-canvas', 'filename'),
+    Input('upload-mask', 'contents'),
+    Input('patch-size-slider', 'value'), 
+    Input('local-radius-slider', 'value'),  
+    Input('data-term-slider', 'value'), 
+    Input('fill-rect', 'value')])
+def inpaint_image(string, image_filename, mask_contents, patch_size, local_radius, data_significance, rect_fill):
     if string:
         data = json.loads(string)
     else:
         raise PreventUpdate
+    
+    # print(data)
 
-    print(data['objects'], file = sys.stderr)
+    image_ID = image_filename.split('/')[3].split('.')[0]
+    image = np.array(Image.open(os.getcwd() + image_filename))
 
-    image_ID = str(uuid.uuid1())
-
-    image_width = int(data['objects'][0]["width"])
-    image_height = int(data['objects'][0]["height"])
+    image_width = image.shape[1]
+    image_height = image.shape[0]
     
     mask_filename = 'masks/' + image_ID + '.png'
+    print(mask_filename)
 
-    mask = np.zeros((image_height, image_width))
-
-    if data['objects'][1]["type"] == "rect" :
-            
-        left = int(data['objects'][1]["left"] * image_width / canvas_width)
-        top = int(data['objects'][1]["top"] * image_width / canvas_width)
-        width = int(data['objects'][1]["width"] * image_width / canvas_width)
-        height = int(data['objects'][1]["height"] * image_width / canvas_width)
-
-        mask = getMask(image_width, image_height, left, top, width, height)
-        # mask=Image.open(os.getcwd() + app.get_asset_url(mask_filename))
-    # print(mask)
-    else :
-        mask =  1 - parse_jsonstring(string, (image_height, image_width))
-    print(mask.shape)
+    if mask_contents is not None :
+        save_image(mask_contents, mask_filename)
+    else : 
+        mask_from_data(string, data, mask_filename, image_width, image_height, canvas_width, rect_fill)
     
-    plt.imsave(os.getcwd() + app.get_asset_url(mask_filename), mask)
+    mask = np.array(Image.open(os.getcwd() + app.get_asset_url(mask_filename)))
+    mask = 1 * (mask == np.max(mask))[:, :, 0]
+    print(mask.shape)
 
-    image_filename = 'test_images/pavage.png'
-    image = Image.open(os.getcwd() + app.get_asset_url(image_filename))
+    # print(mask[0, 0, :])
 
-    # mask = cv2.imread(os.getcwd() + app.get_asset_url('mask2.jpg'),2)
-    # ret, mask = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
-    # print(mask)
-    #plt.imsave(os.getcwd() + app.get_asset_url("mask2.jpg"), mask)
-
-
-    inpainted = inpaint(image, mask, patch_size, local_radius=50)
-
+    inpainted = inpaint(image, mask, patch_size, local_radius = local_radius, data_significance = data_significance)
     inpainted_filename = 'results/' + image_ID + '.png'
-
-    # inpainted_array = Image.fromarray(inpainted)
-    # Image.save(inpainted, "/assets/inpainted.jpeg"
-
     plt.imsave(os.getcwd() + app.get_asset_url(inpainted_filename), inpainted)
 
     return app.get_asset_url(inpainted_filename)
